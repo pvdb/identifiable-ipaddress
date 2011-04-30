2c2a45a0141ab0b65f8fc9d4a67c00cad2f7d808
Peter Vandenberk <pvandenberk@mac.com>
2011-04-30 18:23:43 +0100

    module-ize the test classes, to match autotest mapping conventions

--- test/ipaddress/test_identifiable.rb
+++ test/ipaddress/test_identifiable.rb
@@ -1,40 +1,42 @@
 require 'helper'
 
-class TestIdentifiable < Test::Unit::TestCase
+module TestIpaddress
+  class TestIdentifiable < Test::Unit::TestCase
 
-  context "IPAddress instances" do
+    context "IPAddress instances" do
 
-    setup do
-      @ipaddress = IPAddress('127.0.0.1')
+      setup do
+        @ipaddress = IPAddress('127.0.0.1')
+      end
+
+      should "respond to identity?" do
+        assert @ipaddress.respond_to? "identity?"
+      end
+
+      should "respond to identity" do
+        assert @ipaddress.respond_to? :identity
+      end
+
+      should "respond to identity=" do
+        assert @ipaddress.respond_to? "identity="
+      end
+
     end
 
-    should "respond to identity?" do
-      assert @ipaddress.respond_to? "identity?"
+    context "identifiable IPAddress instances" do
+
+      setup do
+        @ipaddress = IPAddress('127.0.0.1')
+      end
+
+      should "return correct identity" do
+        assert !@ipaddress.identity?
+        @ipaddress.identity = 'localhost'
+        assert_equal 'localhost', @ipaddress.identity
+        assert @ipaddress.identity?
+      end
+
     end
 
-    should "respond to identity" do
-      assert @ipaddress.respond_to? :identity
-    end
-
-    should "respond to identity=" do
-      assert @ipaddress.respond_to? "identity="
-    end
-
-  end
-
-  context "identifiable IPAddress instances" do
-
-    setup do
-      @ipaddress = IPAddress('127.0.0.1')
-    end
-
-    should "return correct identity" do
-      assert !@ipaddress.identity?
-      @ipaddress.identity = 'localhost'
-      assert_equal 'localhost', @ipaddress.identity
-      assert @ipaddress.identity?
-    end
-
-  end
-
-end
+  end # class TestIdentifiable
+end # module TestIpaddress
--- test/ipaddress/test_identities.rb
+++ test/ipaddress/test_identities.rb
@@ -1,36 +1,38 @@
 require 'helper'
 
-class TestIdentities < Test::Unit::TestCase
+module TestIpaddress
+  class TestIdentities < Test::Unit::TestCase
 
-  context "IPAddress class" do
+    context "IPAddress class" do
 
-    should "respond to identity_for?" do
-      assert IPAddress.respond_to? :identity_for?
-    end
+      should "respond to identity_for?" do
+        assert IPAddress.respond_to? :identity_for?
+      end
 
-    should "respond to get_identity" do
-      assert IPAddress.respond_to? :get_identity
-    end
+      should "respond to get_identity" do
+        assert IPAddress.respond_to? :get_identity
+      end
 
-    should "respond to set_identity" do
-      assert IPAddress.respond_to? :set_identity
-    end
-
-  end
-
-  context "IPAddress identities" do
-
-    should "record an IPAddress's identity" do
-
-      ipaddress = '127.0.0.1' ; identity = 'localhost'
-
-      assert !IPAddress.identity_for?(ipaddress)
-      IPAddress.set_identity(ipaddress, identity)
-      assert IPAddress.identity_for?(ipaddress)
-      assert_equal identity, IPAddress.get_identity(ipaddress)
+      should "respond to set_identity" do
+        assert IPAddress.respond_to? :set_identity
+      end
 
     end
 
-  end
+    context "IPAddress identities" do
 
-end
+      should "record an IPAddress's identity" do
+
+        ipaddress = '127.0.0.1' ; identity = 'localhost'
+
+        assert !IPAddress.identity_for?(ipaddress)
+        IPAddress.set_identity(ipaddress, identity)
+        assert IPAddress.identity_for?(ipaddress)
+        assert_equal identity, IPAddress.get_identity(ipaddress)
+
+      end
+
+    end
+
+  end # class TestIdentities
+end # module TestIpaddress
