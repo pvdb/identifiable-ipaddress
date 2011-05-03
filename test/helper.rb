6262e9a1ddca7b6f24aafa3459dd78b30338d9dc
Peter Vandenberk <pvandenberk@mac.com>
2011-05-03 08:37:40 +0100

    some test helper functions (for future use)

--- test/helper.rb
+++ test/helper.rb
@@ -15,4 +15,15 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'identifiable_ipaddress'
 
 class Test::Unit::TestCase
+
+  def random_ipv4_address
+    # FIXME exclude the 255 broadcast address
+    ((1..4).map { Kernel.rand(256) }).join('.')
+  end
+
+  def local_ipv4_address
+    # http://en.wikipedia.org/wiki/IP_address#IPv4_private_addresses
+    (['192', '168'] + (1..2).map { Kernel.rand(256) }).join('.')
+  end
+
 end
